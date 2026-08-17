43961f62ca4e8e85f9287727c607865199a17210
Chikezie Harrison <47220057+cheekezie@users.noreply.github.com>
2026-08-17 18:00:41 +0100

    analytics build fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 
 const queryClient = new QueryClient({
   defaultOptions: {
